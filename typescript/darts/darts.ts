7a3a91d64c47106ad7aa633ebd1b595c15481d70
Max Normand <maxnormand97@gmail.com>
2024-05-31 11:26:24 +0100

    start on JS path
    - update gitingore for js course
    - complete darts.ts

--- typescript/darts/darts.ts
+++ typescript/darts/darts.ts
@@ -1,37 +1,13 @@
+export function score(x: number, y: number): number {
+    const distance = Math.sqrt(x * x + y * y);
 
-    export function score(x: number, y: number): number {
-        // TODO: need to break up the co-oridinates for outer middle and inner circles
-        // then convert each to a rule to make the code nicer
-        if (x === 10 || y === 10) {
-            return 0;
-        }
-        // outer circle
-        // if (x === 0 && y <= 10) {
-        //     return 1;
-        // }
-        // just outside the outer circle
-        if (x >= 7.1 || y >= 7.1 || x <= -7.1 || y <= -7.1) {
-            return 0;
-        }
-        // bullseye
-        if (x === 0 && y === 0) {
-            return 10;
-        }
-        // inner circle
-        if (x <= 0.7 || y <= 0.7) {
-            return 10;
-        }
-        // middle circle
-        if (x <= 5 && y <= 5) {
-            return 5;
-        }
-        // anything in the 5 unit circle should be 5 points
-        if (x <= 5 && y <= 5) {
-            return 5;
-        }
-        // anything in the 1. unit circle should be 10 points
-        if (x <= 1 || y <= 1) {
-            return 10;
-        }
-      return 0;
+    if (distance > 10) {
+        return 0;
+    } else if (distance > 5) {
+        return 1;
+    } else if (distance > 1) {
+        return 5;
+    } else {
+        return 10;
     }
+}
